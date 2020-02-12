521f258d85a61fe22dc19ad995a2830f3d6321e6
David Haen <haendd@msoe.edu>
2020-02-12 15:04:31 -0600

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+import json
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/api/V1.0/get_building_info/<string:building>')
+def building_info(building):
+    with open('buildings.json') as file:
+        building_json = json.load(file)
+        print(building_json[0])
+        for building_object in building_json:
+            print(building)
+            if building_object['name'] == building:
+                return building_object
+    return "Building not found"
+
+
+if __name__ == '__main__':
+    app.run()
